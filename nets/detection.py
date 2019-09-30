5f31bc16801ca09c375bdc3ba87fcadd0700337d
Tao <m466022340@163.com>
2019-09-30 16:19:09 +0800

    fixed function class bug.

--- nets/detection.py
+++ nets/detection.py
@@ -15,6 +15,12 @@ cfg = {
     'clip': True,
     'name': 'VOC',
 }
+num_classes = cfg['num_classes']
+background_label = 0
+top_k = 200
+conf_thresh = 0.01
+nms_thresh = 0.45
+variance = cfg['variance']
 
 class Detect(Function):
     """At test time, Detect is the final layer of SSD.  Decode location preds,
@@ -22,18 +28,8 @@ class Detect(Function):
     scores and threshold to a top_k number of output predictions for both
     confidence score and locations.
     """
-    def __init__(self, num_classes, bkg_label, top_k, conf_thresh, nms_thresh):
-        self.num_classes = num_classes
-        self.background_label = bkg_label
-        self.top_k = top_k
-        # Parameters used in nms.
-        self.nms_thresh = nms_thresh
-        if nms_thresh <= 0:
-            raise ValueError('nms_threshold must be non negative.')
-        self.conf_thresh = conf_thresh
-        self.variance = cfg['variance']
-
-    def forward(self, loc_data, conf_data, prior_data):
+    @staticmethod
+    def forward(ctx, loc_data, conf_data, prior_data):
         """
         Args:
             loc_data: (tensor) Loc preds from loc layers
@@ -45,30 +41,30 @@ class Detect(Function):
         """
         num = loc_data.size(0)  # batch size
         num_priors = prior_data.size(0)
-        output = torch.zeros(num, self.num_classes, self.top_k, 5)
+        output = torch.zeros(num, num_classes, top_k, 5)
         conf_preds = conf_data.view(num, num_priors,
-                                    self.num_classes).transpose(2, 1)
+                                    num_classes).transpose(2, 1)
 
         # Decode predictions into bboxes.
         for i in range(num):
-            decoded_boxes = decode(loc_data[i], prior_data, self.variance) # here change the offset to percentage of the scale
+            decoded_boxes = decode(loc_data[i], prior_data, variance) # here change the offset to percentage of the scale
             # For each class, perform nms
             conf_scores = conf_preds[i].clone()
 
-            for cl in range(1, self.num_classes):
-                c_mask = conf_scores[cl].gt(self.conf_thresh)
+            for cl in range(1, num_classes):
+                c_mask = conf_scores[cl].gt(conf_thresh)
                 scores = conf_scores[cl][c_mask]
                 if scores.size(0) == 0:
                     continue
                 l_mask = c_mask.unsqueeze(1).expand_as(decoded_boxes)
                 boxes = decoded_boxes[l_mask].view(-1, 4)
                 # idx of highest scoring and non-overlapping boxes per class
-                ids, count = nms(boxes, scores, self.nms_thresh, self.top_k)
+                ids, count = nms(boxes, scores, nms_thresh, top_k)
                 output[i, cl, :count] = \
                     torch.cat((scores[ids[:count]].unsqueeze(1),
                                boxes[ids[:count]]), 1)
         flt = output.contiguous().view(num, -1, 5)
         _, idx = flt[:, :, 0].sort(1, descending=True)
         _, rank = idx.sort(1)
-        flt[(rank < self.top_k).unsqueeze(-1).expand_as(flt)].fill_(0)
+        flt[(rank < top_k).unsqueeze(-1).expand_as(flt)].fill_(0)
         return output
--- nets/mobilenetv2.py
+++ nets/mobilenetv2.py
@@ -184,7 +184,7 @@ class MobileNetV2(nn.Module):
         #     nn.Dropout(0.2),
         #     nn.Linear(self.last_channel, n_class),
         # )
-        self.detect = Detect(n_class, 0, 200, 0.01, 0.45)
+        self.detect = Detect()
         self.softmax = nn.Softmax(dim=-1)
         self._initialize_weights()
 
@@ -216,7 +216,7 @@ class MobileNetV2(nn.Module):
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
 
         if self.mode == 'test':
-            output = self.detect(
+            output = self.detect.apply(
                 loc.view(loc.size(0), -1, 4),                   # loc preds
                 self.softmax(conf.view(conf.size(0), -1,
                                 self.num_classes)),                # conf preds
