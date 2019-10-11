e34c74d54db7ce126ae6ef881d3091213c63b7d1
tao <huangtao@g7.com.cn>
2019-10-11 10:29:10 +0800

    add vgg50_student net.

--- nets/__init__.py
+++ nets/__init__.py
@@ -1,4 +1,5 @@
 from .vgg import vgg_module
+from .vgg_student import vgg_student_module
 from .mobilenetv2 import mobilenetv2_module
 
-__all__ = ['vgg_module', 'mobilenetv2_module']
+__all__ = ['vgg_module', 'vgg_student_module', 'mobilenetv2_module']
