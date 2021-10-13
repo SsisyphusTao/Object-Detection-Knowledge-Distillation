80eecd4d199225110ef78cb2ba05b9ccfc80c555
Chandler <chandler@invix.com>
2021-10-13 09:44:24 +0000

    fix: vgg module import bug.

--- odkd/models/backbone/vgg.py
+++ odkd/models/backbone/vgg.py
@@ -2,11 +2,11 @@
 import torch
 import torch.nn as nn
 from typing import Union, List, Dict, Any, cast
-from ._utils import load_state_dict_from_url
+from .._utils import load_state_dict_from_url
 
 __all__ = [
-    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
-    'vgg19_bn', 'vgg19',
+    'VGG', 'vgg16_bn',
+    'vgg19_bn'
 ]
 
 
