ca738ce39750ed8f5bae2192b53745025aac85ca
Chandler <chandler@invix.com>
2021-10-16 09:00:01 +0000

    feat: detect

--- odkd/models/ssdlite.py
+++ odkd/models/ssdlite.py
@@ -1,10 +1,13 @@
 
 """SSDLite detector relative blocks"""
+import torchvision
 import torch
 from torch import nn
+from torchvision.ops import nms
 from math import sqrt
 from itertools import product
 from ._utils import InvertedResidual, SeperableConv2d, _make_divisible
+from .box_utils import decode
 
 
 def create_priorbox(cfg):
@@ -12,7 +15,10 @@ def create_priorbox(cfg):
     feature map.
 
     Args:
-        cfg (dict): SSDLite structure config.
+        cfg (dict): SSDLite config including prior boxes parameters
+    
+    Return:
+        Prior boxes
 
     """
 
@@ -46,22 +52,88 @@ def create_priorbox(cfg):
     return output
 
 
+class Detect(nn.Module):
+    """At test time, Detect is the final layer of SSD.  Decode location preds,
+    apply non-maximum suppression to location predictions based on conf
+    scores and threshold to a top_k number of output predictions for both
+    confidence score and locations.
+
+    Args:
+        cfg: (dict) SSD config including inference parameters
+        prior_data: (tensor) Prior boxes and variances from priorbox layers
+            Shape: [1,num_priors,4]
+    
+    Return:
+        (tensor) Predictions of bounding boxes and scores of each classes
+            Shape: [1,num_classes,topK,5]
+    """
+
+    def __init__(self, cfg, prior_data):
+        super().__init__()
+        self.num_classes = cfg['num_classes']
+        self.top_k = cfg['topK']
+        self.variance = cfg['variance']
+        self.conf_thresh = cfg['conf_thresh']
+        self.nms_thresh = cfg['nms_thresh']
+        self.prior_data = prior_data
+        self.num_priors = self.prior_data.size(0)
+
+    def forward(self, loc_data, conf_data):
+        """
+        Args:
+            loc_data: (tensor) Loc preds from loc layers
+                Shape: [batch,num_priors*4]
+            conf_data: (tensor) Shape: Conf preds from conf layers
+                Shape: [batch*num_priors,num_classes]
+        """
+        num = loc_data.size(0)  # batch size
+ 
+        output = torch.zeros(num, self.num_classes, self.top_k, 5)
+        conf_preds = conf_data.view(num, self.num_priors,
+                                    self.num_classes).transpose(2, 1)
+
+        # Decode predictions into bboxes.
+        for i in range(num):
+            # here change the offset to percentage of the scale
+            decoded_boxes = decode(loc_data[i], self.prior_data, self.variance)
+            # For each class, perform nms
+            conf_scores = conf_preds[i].clone()
+
+            for cl in range(1, self.num_classes):
+                c_mask = conf_scores[cl].gt(self.conf_thresh)
+                scores = conf_scores[cl][c_mask]
+                if scores.size(0) == 0:
+                    continue
+                l_mask = c_mask.unsqueeze(1).expand_as(decoded_boxes)
+                boxes = decoded_boxes[l_mask].view(-1, 4)
+                # idx of highest scoring and non-overlapping boxes per class
+                idx = nms(boxes, scores, self.nms_thresh)[:self.top_k]
+                output[i, cl, :idx.size(0)] = \
+                    torch.cat((scores[idx].unsqueeze(1),
+                               boxes[idx]), 1)
+        flt = output.contiguous().view(num, -1, 5)
+        _, idx = flt[:, :, 0].sort(1, descending=True)
+        _, rank = idx.sort(1)
+        flt[(rank < self.top_k).unsqueeze(-1).expand_as(flt)].fill_(0)
+        return output
+
+
 class SSDLite(nn.Module):
     """SSDlite detect head which only has 3000 anchors
 
     Args:
-        num_classes (int): number of prediction classes.
-        width_mult (:obj:`float`, optional): scale ratio of channel width.
-        div_nearest (:obj:`int`, optional): multiple of channels.
+        cfg (int): SSDLite config
+        width_mult (:obj:`float`, optional): scale ratio of channel width
+        div_nearest (:obj:`int`, optional): multiple of channels
 
     Attributes:
         cfg (dict): default prior boxex config.
     """
 
-    def __init__(self, num_classes, width_mult=1.0, div_nearest=8):
+    def __init__(self, cfg, width_mult=1.0, div_nearest=8):
         super().__init__()
 
-        self.num_classes = num_classes
+        self.num_classes = cfg['num_classes']
 
         def div(x):
             return _make_divisible(x * width_mult, div_nearest)
@@ -88,18 +160,21 @@ class SSDLite(nn.Module):
         ])
         self.classification_headers = nn.ModuleList([
             SeperableConv2d(in_channels=div(576),
-                            out_channels=6 * num_classes, kernel_size=3, padding=1),
+                            out_channels=6 * self.num_classes, kernel_size=3, padding=1),
             SeperableConv2d(in_channels=div(1280),
-                            out_channels=6 * num_classes, kernel_size=3, padding=1),
+                            out_channels=6 * self.num_classes, kernel_size=3, padding=1),
             SeperableConv2d(in_channels=div(512),
-                            out_channels=6 * num_classes, kernel_size=3, padding=1),
+                            out_channels=6 * self.num_classes, kernel_size=3, padding=1),
             SeperableConv2d(in_channels=div(256),
-                            out_channels=6 * num_classes, kernel_size=3, padding=1),
+                            out_channels=6 * self.num_classes, kernel_size=3, padding=1),
             SeperableConv2d(in_channels=div(256),
-                            out_channels=6 * num_classes, kernel_size=3, padding=1),
+                            out_channels=6 * self.num_classes, kernel_size=3, padding=1),
             nn.Conv2d(in_channels=div(64),
-                      out_channels=6 * num_classes, kernel_size=1),
+                      out_channels=6 * self.num_classes, kernel_size=1),
         ])
+
+        self.priors = create_priorbox(cfg)
+        self.detect = Detect(cfg, self.priors)
 
     def forward(self, layers):
         loc = []
@@ -113,6 +188,6 @@ class SSDLite(nn.Module):
         conf = torch.cat([i.view(i.size(0), -1) for i in conf], 1)
 
         if self.training:
-            return loc.view(loc.size(0), -1, 4), conf.view(conf.size(0), -1, self.num_classes)
+            return loc.view(loc.size(0), -1, 4), conf.view(conf.size(0), -1, self.num_classes), self.priors
         else:
-            return
+            return self.detect(loc.view(loc.size(0), -1, 4), conf.view(conf.size(0), -1, self.num_classes))
--- odkd/tests/models/test_ssdlite.py
+++ odkd/tests/models/test_ssdlite.py
@@ -1,5 +1,6 @@
 import torch
 from odkd.models.ssdlite import (
+    Detect,
     SSDLite,
     create_priorbox
 )
@@ -7,6 +8,7 @@ from odkd.tests import (
     config
 )
 
+cfg = config['SSDLite']
 # steps: [16, 32, 64, 100, 150, 300]
 x = [
     torch.randn(1, 576, 19, 19, dtype=torch.float32),
@@ -16,18 +18,27 @@ x = [
     torch.randn(1, 256, 2, 2, dtype=torch.float32),
     torch.randn(1, 64, 1, 1, dtype=torch.float32)
 ]
-num_priors = sum(map(lambda x: x*x, config['SSDLite']['feature_maps_size'])) * len(
-    config['SSDLite']['aspect_ratios'])
-
+num_priors = sum(map(lambda x: x*x, cfg['feature_maps_size'])) * len(
+    cfg['aspect_ratios'])
+location = torch.randn(1, num_priors, 4, dtype=torch.float32)
+confidence = torch.randn(1, num_priors, cfg['num_classes'], dtype=torch.float32)
+priors = torch.randn(num_priors, 4, dtype=torch.float32)
 
 def test_create_priorbox():
-    priors = create_priorbox(config['SSDLite'])
-    print(priors.shape)
-    assert priors.shape == torch.Size([num_priors, 4])
+    _priors = create_priorbox(cfg)
+    print(_priors.shape)
+    assert _priors.shape == priors.shape
 
+def test_detect():
+    detect = Detect(cfg, priors)
+    output = detect(location, confidence)
+    print(output.shape)
+    assert output.shape == torch.Size([1, cfg['num_classes'], cfg['topK'], 5])
 
 def test_ssdlite():
-    head = SSDLite(80)
-    location, config = head(x)
-    print(location.shape, config.shape)
-    assert location.shape[1] == config.shape[1] == num_priors
+    head = SSDLite(cfg)
+    _location, _confidence, _priors = head(x)
+    print(_location.shape, _confidence.shape, _priors.shape)
+    assert _location.shape == location.shape
+    assert _confidence.shape == confidence.shape
+    assert _priors.shape == priors.shape
